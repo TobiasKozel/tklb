5dcf57e31d96ebaa53e179b4b90f0f6fe39cfdd3
TobiasKozel <t.kozel@protonmail.com>
2020-08-20 01:01:37 +0200

    m

--- types/audio/TResampler.h
+++ types/audio/TResampler.h
@@ -1,8 +1,19 @@
 #ifndef TKLB_RESAMPLER
 #define TKLB_RESAMPLER
 
+#include "../../external/SpeexResampler.h"
 namespace tklb {
 
-}
+class Resampler {
+	speexport::SpeexResampler resampler;
+public:
+	Resampler(int rateIn, int rateOut, int channels = 2, int quality = 5) {
+		int err;
+		resampler.init(channels, rateIn, rateOut, quality, &err);
+	}
+
+};
+
+} // namespace
 
 #endif // RESAMPLER
