cae9502cbf7db7e385e18b58fd5edeca3fce4da3
Tobias Kozel <t.kozel@protonmail.com>
2020-08-31 00:40:14 +0200

    testscript for windows

--- tests/runall.sh
+++ tests/runall.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 FAILED=false
 
+unameOut="$(uname -s)"
+case "${unameOut}" in
+	Linux*)     executable=./a.out;;
+	Darwin*)    executable=./a.out;;
+	CYGWIN*)    executable=./a.exe;;
+	MINGW*)     executable=./a.exe;;
+	*)          executable=./a.out
+esac
+
 function test {
 	for f in ./*.cpp
 	do
 		if $1 $f ; then
-			if ./a.out ; then
+			if $executable ; then
 				echo "${1} Passed: ${f}"
 			else
 				FAILED=true
 				echo -e "\e[31m${1}  Error: Test failed for ${f}\e[0m"
 			fi
-			rm ./a.out
+			rm $executable
 		else
 			echo -e "\e[31m${1} Error: Failed to compile ${f}\e[0m"
 			exit
