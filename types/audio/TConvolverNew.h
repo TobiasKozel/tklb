b7c174817977944abd241d12504fb9ecb88323e3
Tobias Kozel <t.kozel@protonmail.com>
2020-09-07 23:03:22 +0200

    convolver init

--- types/audio/TConvolverNew.h
+++ types/audio/TConvolverNew.h
@@ -15,7 +15,8 @@ public:
 	using uint = unsigned int;
 
 private:
-	uint mBlockSize, mSegmentSize, mSegmentCount, mFFTSize;
+	uint mBlockSize, mSegmentSize, mSegmentCount, mFFTComplexSize;
+	uint mInputBufferFill, mCurrentPosition;
 	AudioBuffer<> mComplexIr, mFFTBuffer;
 	std::vector<AudioBuffer<>> mSegments, mSegmentsIR;
 	FFT mFFT;
@@ -23,21 +24,42 @@ private:
 public:
 	// template <typename T>
 	using T = double;
-	void load(const AudioBuffer<>::Buffer<T>& ir, const uint blockSize) {
-		// reset
-		uint length = ir.size();
+	void load(const T* ir, uint irLength, const uint blockSize) {
 
-		// trim silence
+		// trim silence, since longer IR increase CPU usage considerably
 		const T silence = 0.000001;
-		while (length-- && silence < fabs(ir[length])) { }
+		while (--irLength && silence > fabs(ir[irLength])) { }
+		if (irLength == 0) { return; }
 
+		// Figure out how many segments a block is
 		mBlockSize = powerOf2(blockSize);
 		mSegmentSize = 2 * mBlockSize;
-		mSegmentCount = std::ceil(length / T(mBlockSize));
-		mFFTSize = (mSegmentSize / 2) + 1;
+		mSegmentCount = std::ceil(irLength / T(mBlockSize));
+		mFFTComplexSize = (mSegmentSize / 2) + 1;
 
+		// create segment buffers for the input signal
+		mSegments.resize(mSegmentCount);
 		mFFT.resize(mSegmentSize);
 		mFFTBuffer.resize(mSegmentSize, 1);
+
+		// create segment buffers for the impulse response
+		mSegmentsIR.resize(mSegmentCount);
+		for (uint i = 0; i < mSegmentCount; i++) {
+			const uint remaining = std::min(irLength - (i * mBlockSize), mBlockSize);
+			mFFTBuffer.set(0);
+			// Put the segment in the fft buffer
+			mFFTBuffer.set(ir + (i * mBlockSize), 0, remaining);
+			mSegmentsIR[i].resize(mFFTComplexSize, 2); // make space in the result buffer
+			mSegments[i].resize(mFFTComplexSize, 2); // and the input signal buffer
+			mFFT.forward(mFFTBuffer, mSegmentsIR[i]); // save the fft result for each segment
+		}
+
+		mCurrentPosition = 0;
+		mInputBufferFill = 0;
+	}
+
+	void process() {
+
 	}
 
 };
