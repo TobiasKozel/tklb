931476b6c0fa3423db776bdc469b74f3109e683d
TobiasKozel <t.kozel@protonmail.com>
2021-12-17 23:19:28 +0100

    m

--- src/types/audio/resampler/TResamplerSpeex.cpp
+++ src/types/audio/resampler/TResamplerSpeex.cpp
@@ -1,7 +1,7 @@
 
 // If the file gets compiled on it's own
 // we need to get the settings for the speex resampler
-#ifndef TKLBZ_RESAMPLER_SPEEX
+#ifndef _TKLB_RESAMPLER_SPEEX
 	#include "./TResamplerSpeex.hpp"
 #endif
 
