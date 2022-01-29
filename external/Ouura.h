069e4dfe15e2a9b44e5bf45d4ba51176cbbeb81a
TobiasKozel <t.kozel@protonmail.com>
2022-01-29 16:15:58 +0100

    ooura fft copyright notice

--- external/Ouura.h
+++ external/Ouura.h
@@ -1,5 +1,10 @@
 /**
- * TODO copyright info
+ * Slightly altered OOURA FFT Implementation from
+ *
+ * http://www.kurims.kyoto-u.ac.jp/~ooura/fft.html
+ * Copyright Takuya OOURA, 1996-2001
+ * You may use, copy, modify and distribute this code for any purpose (include commercial use) and without fee.
+ * Please refer to this package when you modify this code.
  */
 
 #ifndef OOURA_FFT
