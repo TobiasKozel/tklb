0dea8f89e9ee55407cae998f9758027269750733
Tobias Kozel <t.kozel@example.com>
2022-08-15 15:54:49 +0200

    fix uuid

--- src/util/TUuid.h
+++ src/util/TUuid.h
@@ -5,65 +5,78 @@
 #include <ctime>
 #include <cstdlib>
 
-namespace tklb {
-	class uuid {
+namespace tklb { namespace uuid {
+	static constexpr int DashPos[] = { 9, 14, 19, 24 };
+	static constexpr char CharacterSet[] = "abcdefghijklmnopqrstuvwxyz0123456789";
+	static constexpr int UUIDLength = 36;
 
-	public:
-		static constexpr int UUIDLength = 36;
-		static constexpr int DashPos[] = { 9, 14, 19, 24 };
-		static constexpr char CharacterSet[] = "abcdefghijklmnopqrstuvwxyz0123456789";
+	/**
+	 * @brief Puts a uuid in the provided string,
+	 * @param uuid string of length UUIDLength or UUIDLength + 1
+	 * @param ensureEscaped if true escape character will be written and UUIDLength + 1 is needed!
+	 */
+	void generate(char* uuid, bool ensureEscaped = true)
+	#ifdef TKLB_IMPL
+	{
+		std::srand(std::time(nullptr));
 
-		/**
-		 * @brief Puts a uuid in the provided string
-		 */
-		static void generate(char (&uuid)[UUIDLength]) {
-			std::srand(std::time(nullptr));
-			for (int i = 0; i < UUIDLength; i++) {
-				uuid[i] = CharacterSet[std::rand() % (sizeof(CharacterSet) - 1)];
-			}
-			for (auto i : DashPos) { uuid[i] = '-'; }
+		for (int i = 0; i < UUIDLength; i++) {
+			uuid[i] = CharacterSet[std::rand() % (sizeof(CharacterSet) - 1)];
 		}
+		for (auto i : DashPos) { uuid[i] = '-'; }
 
-		/**
-		 * @brief Check if the provided string is in the uuid format, needs to be lower case
-		 * @param uuid
-		 * @param ensureEscaped Whether to also check for the escape character at the end
-		 * @return true
-		 * @return false
-		 */
-		static bool isValid(const char* uuid, bool ensureEscaped = true) {
-			// First make sure only valid characters are contained
-			for (int i = 0; i < UUIDLength; i++) {
-				const char c = uuid[i];
-				if ((('a' <= c) && (c <= 'z')) || (('0' <= c) && (c <= '9'))) {
-					// valid character
-				} else if (c == '-') {
-					bool correctPosition = false;
-					for (int j : DashPos) {
-						if (j == i) {
-							correctPosition= true;
-							break;
-						}
-					}
-					if (!correctPosition) {
-						return false;
-					}
-				} else {
-					return false; // invalid character
+		if (ensureEscaped) {
+			uuid[UUIDLength] = '\0';
+		}
+	}
+	#endif // TKLB_IMPL
+	;
+
+	/**
+	* @brief Check if the provided string is in the uuid format, needs to be lower case
+	* @param uuid
+	* @param ensureEscaped Whether to also check for the escape character at the end
+	*/
+	static bool isValid(const char* uuid, bool ensureEscaped = true)
+	#ifdef TKLB_IMPL
+	{
+		auto shouldBeDash = [](int i) {
+			for (auto j : DashPos) {
+				if (j == i) { return true; }
+			}
+			return false;
+		};
+		// First make sure only valid characters are contained
+		for (int i = 0; i < UUIDLength; i++) {
+			const char c = uuid[i];
+			if (shouldBeDash(i)) {
+				if (c != '-') {
+					return false;
 				}
+				continue;
 			}
-
-			// Check the excape character if needed
-			if (ensureEscaped && uuid[UUIDLength] != '\0') { return false; }
-			return true;
+			if ((('a' <= c) && (c <= 'z')) || (('0' <= c) && (c <= '9'))) {
+				continue;
+			} else {
+				return false; // invalid character
+			}
 		}
 
-		static bool isValid(const char (&uuid)[UUIDLength]) {
-			// skips length check if the length is matched
-			return isValid(reinterpret_cast<const char*>(uuid), false);
-		}
+		// Check the excape character if needed
+		if (ensureEscaped && uuid[UUIDLength] != '\0') { return false; }
+		return true;
+	}
+	#endif
+	;
 
-	};
-} // tklb
+	bool isValid(const char (&uuid)[UUIDLength])
+	#ifdef TKLB_IMPL
+	{
+		// skips length check if the length is matched
+		return isValid(reinterpret_cast<const char*>(uuid), false);
+	}
+	#endif // TKLB_IMPL
+	;
+} } // tklb::uuid
 
 #endif // _TKLB_UUID
--- tests/TestUuid.cpp
+++ tests/TestUuid.cpp
@@ -3,16 +3,23 @@
 
 int test() {
 	const char* invalid = "asdlkasdlkjsadlkjdsalkasjdasdaaaaaaa";
-	char invalid2[] = "asdlkhhhhhhhhhhjasdlkj";
+	const char* invalid2 = "asdlkhhhhhhhhhhjasdlkj";
 	if (tklb::uuid::isValid(invalid)) {
 		return 1;
 	}
 	if (tklb::uuid::isValid(invalid2)) {
 		return 2;
 	}
+
 	char generate[tklb::uuid::UUIDLength];
 	tklb::uuid::generate(generate);
 	if (!tklb::uuid::isValid(generate)) {
+		return 3;
+	}
+
+	char* generate2 = (char*) malloc(tklb::uuid::UUIDLength + 1);
+	tklb::uuid::generate(generate2);
+	if (!tklb::uuid::isValid(generate2)) {
 		return 3;
 	}
 	return 0;
